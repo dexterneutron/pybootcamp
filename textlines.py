023454b41d0c8b48edc6cb78458462a9176f303d
Felix Lugo <felixdlugo@gmail.com>
2021-09-23 21:14:07 -0400

    Excersise 4 Done

---
+++ textlines.py
@@ -0,0 +1,11 @@
+"""Excersise 4: Write a Python program to count the number 
+of lines in a text file.
+"""
+
+text_file = open("loremipsum.txt", "r")
+
+lines = [line for line in text_file if line !="\n"]
+
+text_file.close()
+
+print(len(lines))
