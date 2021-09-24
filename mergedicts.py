40cd45680571caebfb60059b82b1c70916b2286f
Felix Lugo <felixdlugo@gmail.com>
2021-09-24 00:33:55 -0400

    Excersise 9 Done

---
+++ mergedicts.py
@@ -0,0 +1,18 @@
+"""Excersise 9: Write a Python function to merge two dictionaries.
+"""
+
+def merge_dicts(dict1,dict2):
+    return dict1 | dict2
+
+dict1={
+    'Color' : 'Blue',
+    'Name' : 'Felix',
+    'Profession' : 'Engineer'
+}
+
+dict2={
+    'Fav Food' : 'French Fries',
+    'Fav Sport' : 'F1',
+}
+
+print(merge_dicts(dict1,dict2))
