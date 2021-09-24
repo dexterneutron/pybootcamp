673d14f2cd0c7148a81c8565354da84f5dc7094e
Felix Lugo <felixdlugo@gmail.com>
2021-09-23 20:41:43 -0400

    Excercise 2 Done

---
+++ reversed.py
@@ -0,0 +1,7 @@
+"""Excercise 2: Write a Python program that accepts the user's
+first and last name and prints them in reverse order with a space between them."""
+
+first_name = input("What's your first name?")
+last_name = input("What's your last name?")
+
+print(f"{first_name[ :: -1]} {last_name[ :: -1]}")
